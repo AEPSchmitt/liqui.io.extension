fcab7b523fb2bc8ca78e31fe0bdb4ee5d2c86260
AEPSchmitt <Tanketorskerogn@gmail.com>
2017-07-31 23:19:23 +0200

    Added alpha version of additional graphs.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,48 +4,111 @@ chrome.extension.sendMessage({}, function(response) {
 		clearInterval(readyStateCheckInterval);
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-	  jQuery('.lamp').before(jQuery('<li id="favourites"><a>Favourites</a><ul id="favourites_dropdown" style="display:none;"></ul></li>'));
+	  $('.lamp').before($('<li id="favourites"><a>Favourites</a><ul id="favourites_dropdown" style="display:none;"></ul></li>'));
+		$('.lamp').before($('<li id="test"><a>Test</a>'));
+		$('#test').click(function(evt){
+			//$('iframe')[0].style.width = '50%';
+			//$('#tv_chart_container').append('<div id="yep" style="width:50%"></div>');
+
+			//console.log(sub_frame)
+			//$('#emb_frame').children().not('iframe');
+
+		})
 		//Load favourites from localstorage
 		var favourites = [];
-		chrome.storage.sync.get("favourites", function(favs){
-			if(!jQuery.isEmptyObject(favs)){
-				favourites = favs.favourites;
-				console.log("Favourites loaded from storage as:", favourites);
-				favourites.forEach(function(evt){
-					let url = "https://liqui.io/#/exchange/"+evt+"_BTC";
-					var fav_btn = jQuery('<li>'+evt+'</li>');
-					fav_btn.click(function(e){
-						e.preventDefault();
-						console.log("Redirecting to: " + url);
-						window.location = url;
-						location.reload();
-					});
-					jQuery("#favourites_dropdown").append(fav_btn);
-				})
+		var ctrlActive = false;
+		function removeFavourite(id){
+			var idx = $.inArray(id, favourites);
+			if (idx > -1){
+			  favourites.splice(idx, 1);
+				updateFavourites();
 			}
-		})
-		jQuery('#favourites').click(function(evt){
-			jQuery('#favourites_dropdown').toggle();
-		})
-		jQuery(".market-name").after(jQuery('<button id="add_favourite">Add to Favourites</button>'))
-		jQuery("#add_favourite").click( function(evt){
-				console.log("Clicked")
-					var currency = jQuery(".market-name")[0].innerHTML.replace("/BTC", "");
-					console.log(currency, favourites);
-					console.log($.inArray(currency, favourites));
-					if($.inArray(currency, favourites) < 0){
-						favourites.push(currency);
-						chrome.storage.sync.set({'favourites': favourites}, function() {
-							// Notify that we saved.
-							console.log('Added', currency, "to favourites");
-							chrome.storage.sync.get("favourites", function(favs){
-								console.log(favs);
-							})
+		};
+		function addFavourite(id){
+			var idx = $.inArray(id, favourites);
+			if (idx == -1){
+			  favourites.push(id);
+				updateFavourites();
+			}
+		}
+		function updateFavourites(){
+			chrome.storage.sync.set({'favourites': favourites}, function() {
+				loadFavouritesMenu();
+			});
+		}
+
+		function loadFavouritesMenu(){
+			/*var fav_menu = $('#favourites');
+			while (fav_menu.firstChild) {
+			    fav_menu.removeChild(fav_menu.firstChild);
+			}*/
+
+			console.log("Initialising favourites menu");
+			//$('#favourites').empty();
+			chrome.storage.sync.get("favourites", function(favs){
+				if(!$.isEmptyObject(favs)){
+					favourites = favs.favourites;
+					console.log("Favourites loaded from storage as:", favourites);
+					favourites.forEach(function(c){
+						let url = "https://liqui.io/#/exchange/"+c+"_BTC";
+						var fav_btn = $('<li></li>');
+						var currency_btn = $('<a class="currency_code">'+c+'</a>');
+						currency_btn.click(function(e){
+							if (e.ctrlKey){
+								addGraph(c);
+							} else {
+								e.preventDefault();
+								console.log("Redirecting to: " + url);
+								window.location = url;
+								location.reload();
+							}
 						});
-					}
-				})
-			/*jQuery(".market-name").after(jQuery('<button id="clear_storage">Clear Favourites</button>'))
-			jQuery("#clear_storage").click(function(){
+						fav_btn.append(currency_btn);
+						var close_btn = $('<a class="close"name="'+c+'"style="float: right; z-index:9999;">x</a>');
+						close_btn.click(function(evt){
+							removeFavourite(this.name);
+							//console.log("HELLO", this.parent.text);
+						})
+						fav_btn.append(close_btn);
+						$("#favourites_dropdown").append(fav_btn);
+					})
+				}
+			});
+		}
+		function addGraph(currency){
+			let src = "https://liqui.io/#/exchange/"+currency+"_BTC";
+			let id = 'iframe_' + currency;
+			var frame = $('<iframe id="'+id+'" style="display:none; width:1920px; margin-left: -188px;"</iframe>');
+			$('.top-statistics > .container').append(frame);
+			$('#'+id).attr('src', src);
+			$('#'+id).load(function() {
+				console.log("Iframe loaded");
+				console.log(this);
+				jQuery(this).contents().find('#content > header').hide();
+				jQuery(this).show();
+				jQuery(this).css({'height' : '410px'});
+				//$('.top-statistics > .container').append(save);
+				cleanIframe(this);
+			});
+
+			/*
+			$('#emb_frame').contents().find('#content > header').hide();
+			console.log($('#emb_frame').contents().find('#content > header'));*/
+		}
+		function cleanIframe(frame){
+			console.log("IFRAME!");
+			console.log(frame);
+		}
+		$('#favourites').click(function(evt){
+			$('#favourites_dropdown').toggle();
+		});
+		$(".market-name").after($('<button id="add_favourite">Add to Favourites</button>'))
+		$("#add_favourite").click( function(evt){
+			var currency = $(".market-name")[0].innerHTML.replace("/BTC", "");
+			addFavourite(currency);
+		});
+			/*$(".market-name").after($('<button id="clear_storage">Clear Favourites</button>'))
+			$("#clear_storage").click(function(){
 				chrome.storage.sync.clear(function(evt){
 				  console.log("Cleared storage");
 					chrome.storage.sync.get("favourites", function(favs){
@@ -54,7 +117,20 @@ chrome.extension.sendMessage({}, function(response) {
 				})
 			})*/
 
+		$(window).keydown(function(e){
+    	if (e.keyCode == 17){
+	        ctrlActive = true;
+					$('.currency_code').addClass('ctrl')
+			}
 
+		});
+		$(window).keyup(function(e){
+			if (e.keyCode == 17){
+				$('.currency_code').removeClass('ctrl');
+				ctrlActive = false;
+			}
+		});
+		loadFavouritesMenu();
 		// ----------------------------------------------------------
 
 	}
